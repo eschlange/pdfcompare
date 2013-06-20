47c38777bdac9c50a4cbc51bfe4b1a69921b74f7
eschlange <eschlange@hotmail.com>
2013-06-20 16:44:54 -0500

    Updated the parser to be run via command line by method name rather than hard coding the file names used.

--- xlrs_parser.py
+++ xlrs_parser.py
@@ -1,30 +1,7 @@
 from xlrd import open_workbook,cellname,empty_cell
 
-book = open_workbook('test.xls')
-
-sheet = book.sheet_by_index(0)
-
-# section denotes the section of the spreasheet currently being parsed
-#   it begins with START, moves to the purchase orders, then ends with
-#   with the warehouse section
-section = "START"
-
-# Generic spreadsheet details
-project_number = ""
-project_name = ""
-store_number = ""
-purchase_order_count = 0
-warehouse_order_count = 0
-
-# Column name static variables
-VENDOR_NAME_COL = 0
-PURCHASE_ORDER_NUMBER_COL = 1
-
-purchase_order_tuple_list = []
-warehouse_order_tuple_list = []
-
-# determines the current section of the spreadsheet
 def state_change(current_cell):
+  """ determines the current section of the spreadsheet """
   state = ""
   if current_cell == "Purchase Orders":
     state = "PURCHASE_ORDERS"
@@ -32,8 +9,8 @@ def state_change(current_cell):
     state = "WAREHOUSE_ORDERS"
   return state
 
-# print out a readable set of data for each purchase order and nested item
 def po_print(po_list):
+  """ prints out a readable set of data for each purchase order and nested item """
   for po_tuple in po_list:
     print "PO #:         " + str(po_tuple[0])
     print "Company Name: " + po_tuple[1]
@@ -49,6 +26,7 @@ def po_print(po_list):
       print 
 
 def warehouse_print(warehouse_list):
+  """ prints out a readable set of data for each warehouse order and nested item """
   for warehouse_tuple in warehouse_list:
     print "Warehouse #:         " + str(warehouse_tuple[0])
     print
@@ -63,45 +41,72 @@ def warehouse_print(warehouse_list):
       print
 
 
-# iterate through each row of the spreadsheet
-current_po_item_count = 0
-for row_index in range(sheet.nrows):
-#  print sheet.row_slice(row_index,0)
-  state = state_change(sheet.row_slice(row_index,0)[0].value)
-  if "" != state:
-    section = state
-  elif (not sheet.row_slice(row_index,0)[0].value is empty_cell.value):
+def retrieve_po_and_warehouse_lists(po_file_name):
+  """ Parses a file with a given name (po_file_name) and returns a tuple with the PO list as the first element and the warehouse list as the second element """
+ 
+  book = open_workbook(po_file_name)
+  sheet = book.sheet_by_index(0)
+
+  # section denotes the section of the spreasheet currently being parsed
+  #   it begins with START, moves to the purchase orders, then ends with
+  #   with the warehouse section
+  section = "START"
+
+  # Generic spreadsheet details
+  project_number = ""
+  project_name = ""
+  store_number = ""
+  purchase_order_count = 0
+  warehouse_order_count = 0
+
+  # Column name static variables
+  VENDOR_NAME_COL = 0
+  PURCHASE_ORDER_NUMBER_COL = 1
+
+  purchase_order_tuple_list = []
+  warehouse_order_tuple_list = []
+
+  # iterate through each row of the spreadsheet
+  current_po_item_count = 0
+  for row_index in range(sheet.nrows):
+  #  print sheet.row_slice(row_index,0)
+    state = state_change(sheet.row_slice(row_index,0)[0].value)
+    if "" != state:
+      section = state
+    elif (not sheet.row_slice(row_index,0)[0].value is empty_cell.value):
     
-    if "START" == section:
-      project_number = sheet.row_slice(row_index,0)[2].value
-      project_name = sheet.row_slice(row_index,0)[4].value
-      store_number = sheet.row_slice(row_index,0)[6].value
+      if "START" == section:
+        project_number = sheet.row_slice(row_index,0)[2].value
+        project_name = sheet.row_slice(row_index,0)[4].value
+        store_number = sheet.row_slice(row_index,0)[6].value
     
-    elif "PURCHASE_ORDERS" == section:
-      # if the row constitutes a new PO
-      if sheet.row_slice(row_index,0)[3].value is empty_cell.value:
-        # if purchase order list is empty
-        if purchase_order_tuple_list:
-          purchase_order_count += 1
-        purchase_order_tuple_list.append((sheet.row_slice(row_index,0)[PURCHASE_ORDER_NUMBER_COL].value,sheet.row_slice(row_index,0)[VENDOR_NAME_COL].value,sheet.row_slice(row_index,0)[2],[]))
+      elif "PURCHASE_ORDERS" == section:
+        # if the row constitutes a new PO
+        if sheet.row_slice(row_index,0)[3].value is empty_cell.value:
+          # if purchase order list is empty
+          if purchase_order_tuple_list:
+            purchase_order_count += 1
+          purchase_order_tuple_list.append((sheet.row_slice(row_index,0)[PURCHASE_ORDER_NUMBER_COL].value,sheet.row_slice(row_index,0)[VENDOR_NAME_COL].value,sheet.row_slice(row_index,0)[2],[]))
       
-      # else the row is the seperate items for a PO
-      else:
-        purchase_order_tuple_list[purchase_order_count][3].append((sheet.row_slice(row_index,0)[0].value,sheet.row_slice(row_index,0)[1].value,sheet.row_slice(row_index,0)[2],sheet.row_slice(row_index,0)[3].value,sheet.row_slice(row_index,0)[4].value,sheet.row_slice(row_index,0)[5])) 
+        # else the row is the seperate items for a PO
+        else:
+          purchase_order_tuple_list[purchase_order_count][3].append((sheet.row_slice(row_index,0)[0].value,sheet.row_slice(row_index,0)[1].value,sheet.row_slice(row_index,0)[2],sheet.row_slice(row_index,0)[3].value,sheet.row_slice(row_index,0)[4].value,sheet.row_slice(row_index,0)[5])) 
     
-    elif "WAREHOUSE_ORDERS" == section:
-       # if the row constitutes a new warehouse
-      if sheet.row_slice(row_index,0)[1].value is empty_cell.value:
-        if warehouse_order_tuple_list:
-          warehouse_order_count += 1
-        warehouse_order_tuple_list.append(((sheet.row_slice(row_index,0)[0].value),[]))
-      else: 
-        warehouse_order_tuple_list[warehouse_order_count][1].append((sheet.row_slice(row_index,0)[0].value,sheet.row_slice(row_index,0)[1].value,sheet.row_slice(row_index,0)[2],sheet.row_slice(row_index,0)[3].value,sheet.row_slice(row_index,0)[4].value,sheet.row_slice(row_index,0)[5],sheet.row_slice(row_index,0)[6]))
+      elif "WAREHOUSE_ORDERS" == section:
+        # if the row constitutes a new warehouse
+        if sheet.row_slice(row_index,0)[1].value is empty_cell.value:
+          if warehouse_order_tuple_list:
+            warehouse_order_count += 1
+          warehouse_order_tuple_list.append(((sheet.row_slice(row_index,0)[0].value),[]))
+        else: 
+          warehouse_order_tuple_list[warehouse_order_count][1].append((sheet.row_slice(row_index,0)[0].value,sheet.row_slice(row_index,0)[1].value,sheet.row_slice(row_index,0)[2],sheet.row_slice(row_index,0)[3].value,sheet.row_slice(row_index,0)[4].value,sheet.row_slice(row_index,0)[5],sheet.row_slice(row_index,0)[6]))
 
-# Uncomment the following two lines to print out the stored PO and warehouse data
-#po_print(purchase_order_tuple_list)
-#warehouse_print(warehouse_order_tuple_list)
+  # Uncomment the following two lines to print out the stored PO and warehouse data
+  #po_print(purchase_order_tuple_list)
+  #warehouse_print(warehouse_order_tuple_list)
 
-print "Completed for Project [" + project_name + "], [# " + str(project_number) + "], Store #" + str(store_number)
-print "Purchase order count: " + str(purchase_order_count)
-print "Warehouse order count: " + str(warehouse_order_count)
+  print "Completed for Project [" + project_name + "], [# " + str(project_number) + "], Store #" + str(store_number)
+  print "Purchase order count: " + str(purchase_order_count)
+  print "Warehouse order count: " + str(warehouse_order_count)
+
+  return purchase_order_tuple_list, warehouse_order_tuple_list
