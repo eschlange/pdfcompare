119daf18c3432db12b1fd7b85a66bf4b544b7ea8
eschlange <eschlange@hotmail.com>
2013-06-21 10:40:49 -0500

    Added parsing of proposed orders and a print function. There is new a need to map the PO and warehouse lists to the proposed orders.

--- xlrs_parser.py
+++ xlrs_parser.py
@@ -7,6 +7,8 @@ def state_change(current_cell):
     state = "PURCHASE_ORDERS"
   elif current_cell == "Warehouse Orders":
     state = "WAREHOUSE_ORDERS"
+  elif current_cell == "DESIGN ID":
+    state = "PROPOSED_ORDERS"
   return state
 
 def po_print(po_list):
@@ -24,6 +26,19 @@ def po_print(po_list):
       print "    QTY Ordered:     " + str(item_details[4])
       print "    QTY UOM:         " + item_details[5].value
       print 
+
+def proposed_order_print(proposed_order_list):
+  """ prints out a readable set of data for each warehouse order and nested item """
+  for item_details in proposed_order_list:
+    print "    Design ID:         " + item_details[0]
+    print "    Mapping Status:    " + item_details[1]
+    print "    Revit Description: " + item_details[2]
+    print "    Category:          " + str(item_details[3])
+    print "    Quantity:          " + str(item_details[4])
+    print "    Coverage Unit:     " + item_details[5]
+    print "    Responsibility:    " + item_details[6]
+    print "    Comments:          " + item_details[7]
+    print
 
 def warehouse_print(warehouse_list):
   """ prints out a readable set of data for each warehouse order and nested item """
@@ -50,7 +65,7 @@ def retrieve_po_and_warehouse_lists(po_file_name):
 
   # section denotes the section of the spreasheet currently being parsed
   #   it begins with START, moves to the purchase orders, then ends with
-  #   with the warehouse section
+  #   the warehouse section
   section = "START"
 
   # Generic spreadsheet details
@@ -115,3 +130,60 @@ def retrieve_po_and_warehouse_lists(po_file_name):
 def retrieve_proposed_orders_lists(po_file_name):
   print "Retrieving proposed orders list from file: " + po_file_name + " ..."
   # TODO add implementation
+  """ Parses a file with a given name (po_file_name) and returns a tuple with the PO list as the first element and the warehouse list as the second element """
+
+  print "Retrieving PO and Warehouse orders and items lists"
+  book = open_workbook(po_file_name)
+  sheet = book.sheet_by_index(0)
+
+  # section denotes the section of the spreasheet currently being parsed
+  #   it begins with START, then ends with the proposed orders section
+  section = "START"
+
+  # Generic spreadsheet details
+  project_number = ""
+  project_type = ""
+  store_number = ""
+  store_name = ""
+
+  proposed_order_count = 0
+
+  # Column name static variables
+  DESIGN_ID_COL = 0
+  MAPPING_STATUS_COL = 1
+  REVIT_DESCRIPTION_COL = 2
+  CATEGORY_COL = 3
+  QUANTITY_COL = 4
+  COVERAGE_UNIT_COL = 5
+  RESPONSIBILITY_COL = 6
+  COMMENTS_COL = 7
+
+  proposed_order_tuple_list = []
+
+  # Iterate through each row of the spreadsheet
+  current_po_item_count = 0
+  
+  # Pull general details for the proposed orders
+  project_number = sheet.row_slice(0,0)[1].value
+  project_type = sheet.row_slice(3,0)[1].value
+  store_number = sheet.row_slice(2,0)[1].value
+  store_name = sheet.row_slice(1,0)[1].value
+
+  for row_index in range(sheet.nrows):
+    state = state_change(sheet.row_slice(row_index,0)[0].value)
+    if "" != state:
+      section = state
+
+    elif (not sheet.row_slice(row_index,0)[0].value is empty_cell.value):
+      if "PROPOSED_ORDERS" == section:
+        # if proposed order list is empty
+        if proposed_order_tuple_list:
+          proposed_order_count += 1
+        proposed_order_tuple_list.append((sheet.row_slice(row_index,0)[DESIGN_ID_COL].value,sheet.row_slice(row_index,0)[MAPPING_STATUS_COL].value,sheet.row_slice(row_index,0)[REVIT_DESCRIPTION_COL].value,sheet.row_slice(row_index,0)[CATEGORY_COL].value,sheet.row_slice(row_index,0)[QUANTITY_COL].value,sheet.row_slice(row_index,0)[COVERAGE_UNIT_COL].value,sheet.row_slice(row_index,0)[RESPONSIBILITY_COL].value,sheet.row_slice(row_index,0)[COMMENTS_COL].value,[]))
+
+  # Uncomment the following two lines to print out the stored PO and warehouse data
+  proposed_order_print(proposed_order_tuple_list)
+
+  print "Completed retrieval of proposed orders for Project type [" + project_type + "], [# " + str(project_number) + "], Store #" + str(store_number)
+  print "Proposed order count: " + str(proposed_order_count)
+
